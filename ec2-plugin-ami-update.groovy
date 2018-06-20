0837ffa1d7aeeacac510e644b6aeec0e3c0c5719
Giorgio Sironi <g.sironi@elifesciences.org>
2018-06-20 14:49:10 +0100

    Unify duplicated script

--- ec2-plugin-ami-update.groovy
+++
@@ -1,8 +0,0 @@
-import jenkins.model.Jenkins
-
-Jenkins jenkins = Jenkins.getInstance()
-def ami = jenkins.clouds[0].templates[0].ami
-println("Current AMI: ${ami}")
-
-jenkins.clouds[0].templates[0].ami = 'ami-0d0bb287bec9a4f8a'
-jenkins.save()
--- jenkinsfiles/ec2-plugin-ami-update.groovy
+++ jenkinsfiles/ec2-plugin-ami-update.groovy
@@ -2,7 +2,7 @@ import jenkins.model.Jenkins
 
 Jenkins jenkins = Jenkins.getInstance()
 def ami = jenkins.clouds[0].templates[0].ami
-echo "Current AMI: ${ami}"
+println("Current AMI: ${ami}")
 
 jenkins.clouds[0].templates[0].ami = 'ami-0d0bb287bec9a4f8a'
 jenkins.save()
