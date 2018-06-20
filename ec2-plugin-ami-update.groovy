7cdd90a2475c9e9fc4100085f75c76d4c9bbaeb4
Giorgio Sironi <g.sironi@elifesciences.org>
2018-06-20 14:44:21 +0100

    Substitute echo, a pipeline step, with generic println Groovy procedure

---
+++ ec2-plugin-ami-update.groovy
@@ -0,0 +1,8 @@
+import jenkins.model.Jenkins
+
+Jenkins jenkins = Jenkins.getInstance()
+def ami = jenkins.clouds[0].templates[0].ami
+println("Current AMI: ${ami}")
+
+jenkins.clouds[0].templates[0].ami = 'ami-0d0bb287bec9a4f8a'
+jenkins.save()
