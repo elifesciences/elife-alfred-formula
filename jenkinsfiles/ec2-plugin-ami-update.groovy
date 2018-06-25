9db835955eadab01c7d0d89b7f6f490f9d4e9aa2
Giorgio Sironi <g.sironi@elifesciences.org>
2018-06-25 14:15:26 +0100

    Improve output of AMI update script

--- jenkinsfiles/ec2-plugin-ami-update.groovy
+++ jenkinsfiles/ec2-plugin-ami-update.groovy
@@ -1,10 +1,11 @@
 import jenkins.model.Jenkins
 
-def amiId = args[0]
+def newAmiId = args[0]
 
 Jenkins jenkins = Jenkins.getInstance()
-def ami = jenkins.clouds[0].templates[0].ami
-println("Current AMI: ${ami}")
+def oldAmiId = jenkins.clouds[0].templates[0].ami
+println("Old AMI: ${oldAmiId}")
 
-jenkins.clouds[0].templates[0].ami = amiId
+jenkins.clouds[0].templates[0].ami = newAmiId
 jenkins.save()
+println("New AMI: ${newAmiId}")
