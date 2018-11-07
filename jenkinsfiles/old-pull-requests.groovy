31fa16b90fbf47bbae478ee1c5e711d272e699c6
Giorgio Sironi <g.sironi@elifesciences.org>
2018-11-07 17:20:51 +0000

    Introduce job to list old pull request of elife-xpub

---
+++ jenkinsfiles/old-pull-requests.groovy
@@ -0,0 +1,12 @@
+import jenkins.model.Jenkins
+
+project = args[0]
+
+Jenkins jenkins = Jenkins.getInstance()
+folder = jenkins.getItemByFullName("pull-requests-projects")
+projectFolder = folder.getItemByProjectName(project)
+for (pullRequest in projectFolder.getItems()) {
+    if (!pullRequest.buildable) {
+        println(pullRequest.getName() - "PR-")
+    }
+}
