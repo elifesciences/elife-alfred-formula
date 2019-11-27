a62cca2a05a3fea48b82c0fcccaedd5595281260
Giorgio Sironi <g.sironi@elifesciences.org>
2019-11-27 09:41:57 +0000

    Cater for missing elife-xpub PR folder
    Due to no PRs being open.

--- jenkinsfiles/old-pull-requests.groovy
+++ jenkinsfiles/old-pull-requests.groovy
@@ -5,6 +5,9 @@ project = args[0]
 Jenkins jenkins = Jenkins.getInstance()
 folder = jenkins.getItemByFullName("pull-requests-projects")
 projectFolder = folder.getItemByProjectName(project)
+if (!projectFolder) {
+    return;
+}
 for (pullRequest in projectFolder.getItems()) {
     if (!pullRequest.buildable) {
         println(pullRequest.getName() - "PR-")
